67c81885731be483dba8987986d7402212edef59
Gaston Jouglard <gastonjouglard.10@gmail.com>
2020-09-22 19:25:08 -0300

    Agregando archivos requeridos para deployar en heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ejercicioCAP.wsgi --log-file -
--- ejercicioCAP/settings.py
+++ ejercicioCAP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,3 +127,18 @@ STATIC_URL = '/static/'
 #The only mandatory configuration is the EASY_MAPS_GOOGLE_KEY variable:
 EASY_MAPS_GOOGLE_KEY = 'AIzaSyB6sagFOK4RCY10pC69A6TRvcDai8_nbRU'
 EASY_MAPS_LANGUAGE = 'es'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
